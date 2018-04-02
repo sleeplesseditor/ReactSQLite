648a726e9b8d4027952db100ab40c46ea3a10c2d
sleeplesseditor <josh@bryson.me.uk>
2018-04-02 13:23:12 +0100

    Basic Table component setup

--- src/components/SQLTable.js
+++ src/components/SQLTable.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class SQLTable extends Component {
     render() {
         return (
             <div>
-                Table
+                <table className="table table-hover census-table">
+                    <thead>
+                        <tr>
+                            <th>Value</th>
+                            <th>Count</th>
+                            <th>Average Age</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {}
+                    </tbody>
+                </table>
             </div>
         )
-    }
-};
+    } 
+}
 
 export default SQLTable;
