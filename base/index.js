0d4ad589c8a34d5a8511bb7b39f559b55fb11cb3
sleeplesseditor <josh@bryson.me.uk>
2018-04-05 22:34:56 +0100

    Early attempt at JSON based solution to SQLite issue

--- base/index.js
+++ base/index.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-//Open Database
+//Open Database in read only mode for safety
 let db = new sqlite3.Database('./us-census.db', sqlite3.OPEN_READONLY, (err) => {
    if (err) {
        console.error(err.message);
@@ -8,9 +8,15 @@ let db = new sqlite3.Database('./us-census.db', sqlite3.OPEN_READONLY, (err) => 
    console.log('Connected to US Census Database.');
 });
 
-/*//Query Database First Col
-db.get(sql, )
+//Select Column Headers for Selector Component
 
 
+//Close database
+db.close((err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Close connection to US Census Database.');
+});
 
-module.exports = new Database();*/
+//SQLite AVG Function
